d856ff5299b6c6a9a5fa8d37e29949f17dc6bf9b
utkarsh24082006 <ubgirigosavi24@gmail.com>
2026-06-17 10:52:42 +0530

    Day 2 - loops and functions complete

---
+++ Day2/Repeat.py
@@ -0,0 +1,4 @@
+i = 15
+while i != 0 :
+    print("sale tu kya dholakiya hai kya")
+    i = i-1
---
+++ Day2/repeat1.py
@@ -0,0 +1,18 @@
+def main():
+    number = get_number()
+    yeah(number)
+
+
+def get_number():
+    while True:
+        n = int(input( "What's n ?" ))
+        if n > 0:
+            break
+        return n
+
+def yeah(n):
+    for _ in range(n):
+        print("yeah")
+
+main()    
+    
